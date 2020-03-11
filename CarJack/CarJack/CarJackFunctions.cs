cf405124c4bf8fcc305626b1de708880a2402b75
Eric Leyder <leydere@ferris.edu>
2020-03-11 16:12:32 -0400

    Removed method that was only referenced in unit tests and never used elsewhere; removed nonsense comment too

--- CarJack/CarJack/CarJackFunctions.cs
+++ CarJack/CarJack/CarJackFunctions.cs
@@ -34,39 +34,6 @@ namespace CarJack
             else return "bad";
             
         }
-        /// <summary>
-        /// <returns>Value returned signifies miles driven for that particular trip in a given year.</returns>
-        public static double CalculateInterest(string x, int carPrice)
-        {
-            double interestRateN = 0.00;
-            switch (x)
-            {
-                case "great":
-                    interestRateN = 6.52;
-                    break;
-                case "good":
-                    interestRateN = 7.36;
-                    break;
-                case "fair":
-                    interestRateN = 8.52;
-
-                    break;
-                case "okay":
-                    interestRateN = 9.75;
-
-                    break;
-                case "belowAverage":
-                    interestRateN = 10.35;
-                    break;
-                case "bad":
-                    interestRateN = 12.75;
-                    break;
-            }
-            double paymentsPerMonth = ((carPrice / 120) + ((carPrice / 120)*(interestRateN / 100)));
-            return paymentsPerMonth;
-        }
-
-
 
         public static bool CheckIfFree(int x)
         {
